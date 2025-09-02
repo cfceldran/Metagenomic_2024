fbb4c350c405fbaa331b2d2ee56dcdcdd2e15bc7
Carlos Celdrán <cfceldran@gmail.com>
2025-09-02 13:09:21 +0200

    lasso_data_cleanUp.R

--- lasso_dataCleanUp.R
+++ lasso_dataCleanUp.R
@@ -1,36 +1,37 @@
 ###LASSO DATA CLEAN-UP###
 
-##este script carga los datos necesarios para que los script de lasso funcionen. 
-## Se asume que se ha corrido data_cleanUp antes que este. Sino hay que definir el codigo: codigo <- "XXX_###" #e.g.: sem_16S
+## this Scrpt loads pertinent data for LASSO analysis. 
+## data_cleanUp should had been run befor this one. Otherwise code should be defined: codigo <- "XXX_###" #e.g.: sem_16S
 
 
 
 
-##Datos asignación taxonómica
+##Taxonomic assignation
 
-  #fichero de asignación taxonómica
-  asv_to_otu <- paste0("asv_to_otu/asv_to_otu_", codigo, ".xlsx")
+  #load ASV taxonomic and OTU assignation
+  asv_to_otu <- paste0("asv_to_otu/asv_to_otu_", codigo, ".xlsx") #in my case I have the taxonomic assignation of ASV samed with the 'codigo'
+  otu_genus <- readxl::read_excel(asv_to_otu) 
   
-  #datos taxonómicos
-  otu_genus <- readxl::read_excel(asv_to_otu) #CAMBIAR NOMBRE
-  #con esto seleccionamos la clasificación taxonómica más completa para cada OTU
-  otu_genus <- otu_genus %>% select(c(OTU, Kingdom, Phylum, Class, Order, Family, Genus, Species)) %>%
-    mutate(num_taxa_filled = rowSums(!is.na(across(Kingdom:Genus)) & across(Kingdom:Genus) != "")) %>%
+  #This code will asign each OTU with a taxon extracted from the ASVs that form the OTU. It will assign the most complete taxonomic asignation.
+  # as species classification can be variable (in case there is one), we are maintainig the specie assignation but mark it between parentesis to clarify uncertanty.
+  
+otu_genus <- otu_genus %>% select(c(OTU, Kingdom, Phylum, Class, Order, Family, Genus, Species)) %>%                
+    mutate(num_taxa_filled = rowSums(!is.na(across(Kingdom:Genus)) & across(Kingdom:Genus) != "")) %>%               # num_taxa_filled counts in what dept tax assign is. e.g.: if Genus is assigned then num_taxa_filled = 6
     group_by(OTU) %>%
-    slice_max(num_taxa_filled, with_ties = TRUE) %>% #with_ties: si hay un empate guarda las dos
-    unique() %>% #eliminamos si el empate da resultados que son iguales (misma clasificación)
+    slice_max(num_taxa_filled, with_ties = TRUE) %>%                                                                 # with_ties= TRUE: will save both if there is a tie
+    unique() %>%                                                                                                     # delete if the tie is the same taxonomic assignation
     mutate(
       Species = ifelse(
-        length(unique(Species)) > 1,  # Si hay más de una especie, incluyendo si está vacía
-        paste0("(", paste(unique(Species[!is.na(Species) & Species != ""]), collapse = " / "), ")"), #si
-        unique(Species[!is.na(Species) & Species != ""])  # else: si solo hay una especie, la mantenemos tal cual
+        length(unique(Species)) > 1,                                                                                  # if more than 1 spp class, includying if there is empty ones
+        paste0("(", paste(unique(Species[!is.na(Species) & Species != ""]), collapse = " / "), ")"), 
+        unique(Species[!is.na(Species) & Species != ""])                                                              # else: if spp class is consistent, its maintained.
       )
     ) %>%
-    unique() %>% #resolvemos los empates que se han resuelto uniendo nombres
+    unique() %>%                                                                                                      # resolve new ties
     ungroup() %>%
-    select(-num_taxa_filled) #elimina la columna que hemos creado
-  
-  #añadimos organización taxonómica de forma jerárquica
+    select(-num_taxa_filled)                                                                                          # delete temporal column
+
+  #add OTU assigment jeraquically
   otu_taxa <- data.frame(
     OTU_id = trimws(paste0("OTU", otu_genus$OTU)),
     Taxonomy = ifelse(
@@ -39,37 +40,29 @@
              ifelse(!is.na(otu_genus$Family), otu_genus$Family,
                     ifelse(!is.na(otu_genus$Order), otu_genus$Order,
                            ifelse(!is.na(otu_genus$Class), otu_genus$Class,
-                                  ifelse(!is.na(otu_genus$Phylum), paste("Unknown", otu_genus$Phylum),
+                                  ifelse(!is.na(otu_genus$Phylum), otu_genus$Phylum,
                                          "Unknown bacteria")))))),
     stringsAsFactors = FALSE
   )
 
 
 
-#datos fenotípicos
-  
-  #datos fenotípicos:
-  rawData <- readxl::read_excel("C:/Users/carlos.fceldran/OneDrive - Universidad Rey Juan Carlos/2024/Data/MedidasCultivo/MedidasLupinus_2023_2024.xlsx")
-  growth <- read.csv("C:/Users/carlos.fceldran/OneDrive - Universidad Rey Juan Carlos/2024/Data/MedidasCultivo/growthRates_lm.csv")
-  
-  allData <- left_join(rawData, growth, by = c("pop_code", "sample"))
-  
-  medidas <- allData[, c("pop_code", "sowing","day_till_flower", "RWC", "dry_weight", 
-                         "root_length_cm",	"root_collar_mm",	"N_second_roots", 
-                         "N_branch",	"N_fruits",	"N_seeds",	"seed_mg", 
-                         "shoot_mass",	"root_mass",	"root_shoot", "slope_day")]
-  
-  medidas <- medidas %>%
-    filter(sowing =="si") %>%
-    mutate(across(3:ncol(.), as.numeric))
-  
-  #agrupamos haciendo la media: un solo dato por poblacion
-  medidas <- medidas %>% group_by(pop_code) %>% 
-    summarise(across(where(is.numeric), mean, na.rm = TRUE))
-  
-  #seleccionamos solo datos fenotípicos
-  vars <- medidas %>% select_if(is.numeric)%>% colnames()
+
+#Response Variable
+
+  #In my case I am using OTU abundances as the predictive variable.
+    
+    #Load Response variable. In my case: phenotypic data
+    medidas <- readxl::read_excel("phenotipicData.xslx")
+    
+    #You need to have one response variable per sample. In my case each sample had several individuals, thus several measurements of the same trait, so I made the average of the trait.
+    medidas <- medidas %>% group_by(pop_code) %>% 
+      summarise(across(where(is.numeric), mean, na.rm = TRUE))
+    
+    #here I make an object that holds the colnames of the phenotypic traits
+    vars <- medidas %>% select_if(is.numeric)%>% colnames()
   
   
   
+
   
